07a6dd81243d3c0e995d3e9a319125098dce9b1f
r0n1-putr4 <82272376+r0n1-putr4@users.noreply.github.com>
2024-12-10 22:38:26 +0700

    Update Code

--- app/src/main/java/roni/putra/newsmi2b/MainActivity.kt
+++ app/src/main/java/roni/putra/newsmi2b/MainActivity.kt
@@ -5,15 +5,10 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     private lateinit var btnSignUp: Button
-<<<<<<< HEAD
     private lateinit var btnLogin: Button
-=======
->>>>>>> origin/main
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,21 +16,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         btnSignUp = findViewById(R.id.btnSignUp)
-<<<<<<< HEAD
         btnLogin = findViewById(R.id.btnLogin)
-=======
->>>>>>> origin/main
 
         btnSignUp.setOnClickListener {
             startActivity(Intent(this@MainActivity, RegisterActivity::class.java))
         }
 
-<<<<<<< HEAD
         btnLogin.setOnClickListener {
             startActivity(Intent(this@MainActivity, LoginActivity::class.java))
         }
-=======
->>>>>>> origin/main
     }
 
 
